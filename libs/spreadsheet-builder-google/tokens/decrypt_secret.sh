d7a944363076c16d71c0a5442bc5b99a024c922a
Vladimir Orany <vladimir@orany.cz>
2020-05-01 21:14:48 +0200

    using the path from the root for the decrypt files

--- libs/spreadsheet-builder-google/tokens/decrypt_secret.sh
+++ libs/spreadsheet-builder-google/tokens/decrypt_secret.sh
@@ -2,4 +2,5 @@
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
---output StoredCredential StoredCredential.gpg
+--output libs/spreadsheet-builder-google/tokens/StoredCredential \
+libs/spreadsheet-builder-google/tokens/StoredCredential.gpg
